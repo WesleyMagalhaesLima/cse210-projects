66d060cd72b9db52ca596b504e841c0fb05fac3a
Wesley Magalhães Lima <wesleymagalhaeslima@gmail.com>
2024-11-11 23:08:09 -0300

    Finished learning activity - C# Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //CORE REQUIREMENT #1
+
+        // Console.Write("What is the magic number? ");
+        // string answerMagicNumber = Console.ReadLine();
+        // int magicNumber = int.Parse(answerMagicNumber);
+
+        // Console.Write("What is your guess? ");
+        // string answerGuessNumber = Console.ReadLine();
+        // int guessNumber = int.Parse(answerGuessNumber);
+
+        // if (magicNumber > guessNumber)
+        // {
+        //     Console.WriteLine("Higher");
+        // }
+        // else if (magicNumber < guessNumber)
+        // {
+        //     Console.WriteLine("Lower");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("You guessed it!");
+        // }
+
+
+        //CORE REQUIREMENT #2 AND #3
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 11);
+        int guessNumber = 0;
+
+        while (magicNumber != guessNumber)
+        {
+            Console.Write("What is your guess? ");
+            string answerGuessNumber = Console.ReadLine();
+            guessNumber = int.Parse(answerGuessNumber);
+            
+            if (magicNumber > guessNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guessNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }    
+
     }
 }
