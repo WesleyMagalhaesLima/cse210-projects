ffcd36989b93cbe536c08627197647f453bc71c4
Wesley Magalhães Lima <wesleymagalhaeslima@gmail.com>
2024-11-15 11:18:58 -0300

    Finished learning activity - C# Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int favoriteNumber = PromptUserNumber();
+        int square = SquareNumber(favoriteNumber);
+        DisplayResult(userName, square);
+    }
+
+    static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int favoriteNumber = int.Parse(Console.ReadLine());
+        return favoriteNumber;
+    }
+
+    static int SquareNumber(int favoriteNumber)
+    {
+        int square = favoriteNumber * favoriteNumber;
+        return square;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.Write($"{name}, the square of your number is {square}.");
     }
 }
